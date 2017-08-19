11d566f3a2a5c0f18dcf280da0f7e69df98d87d2
tim-win <tim@timsthebomb.com>
2017-08-19 00:12:13 -0400

    Ok, lets get this show on the road.

---
+++ raspi_maker/__init__.py
@@ -0,0 +1,4 @@
+"""Raspi maker - make nice sd and thumb drive for raspi provisioning."""
+
+def main(args):
+    return 0
---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Initial entrypoint."""
+from raspi_maker import main
+import sys
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
